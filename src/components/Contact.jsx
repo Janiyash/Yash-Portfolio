05e6668d54885ebd590c32439a83b111c2bf5fb1
Yash Jani <janiyash0911@gmail.com>
2026-09-23 14:16:21 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,7 +17,7 @@ const socials = [
     icon: <FaLinkedin className="text-2xl" />,
     label: "LinkedIn",
     value: "linkedin.com/in/jani-yash001/",
-    href: "https://www.linkedin.com/in/jani-yash/",
+    href: "https://www.linkedin.com/in/jani-yash001/",
     color: "blue",
   },
   {
